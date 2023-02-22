6b3ab556eec9d7223c33b7137f3731c1bb85074b
YehorMakeiev <yehor.makeiev@gmail.com>
2023-02-22 16:48:20 +0200

    in progress

---
+++ hw5_5.py
@@ -0,0 +1,13 @@
+def sanitize_phone_number(phone):
+    new_phone = (
+        phone.strip()
+        .removeprefix("+")
+        .replace("(", "")
+        .replace(")", "")
+        .replace("-", "")
+        .replace(" ", "")
+    )
+    return new_phone
+
+
+def get_phone_numbers_for_countries(list_phones):
